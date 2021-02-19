2dac5026805bae8d8e803630041ff7f33099f571
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2021-02-19 14:17:09 +0100

    update

--- app.R
+++ app.R
@@ -6,52 +6,52 @@ library("hrbrthemes")
 
 ui <- fluidPage(
   titlePanel("Microbiologia Alimenti: Carte di controllo Proficiency Test"),
-  
-  sidebarLayout(
+
+
+sidebarLayout(
     sidebarPanel(
       helpText(""),
+
+      selectInput("s", label="Sede", 
+                  choices = c("Sede Territoriale di Bergamo", "Sede Territoriale di Sondrio"), selected= ""), 
       
-      selectInput("mp", 
+      selectInput("mp",
                   label = "MMPP",
-                  choices = c( "MP 01/028", "MP 01/033", "MP 01/043", "MP 01/054",
-                               "MP 01/089", "MP 01/092", "MP 01/093", "MP 01/101", "MP 01/102",
-                               "MP 01/110", "MP 01/111", "MP 01/112", "MP 01/142", "MP 01/144",
-                              "MP 01/145", "MP 01/171","MP 01/194", "MP 02/073", "MP 02/254"),
-                  
-                  
-                  selected = "")
+                  choices = c("MP 01/028", "MP 01/033", "MP 01/043", "MP 01/054", "MP 01/091", 
+                              "MP 01/089", "MP 01/092", "MP 01/093", "MP 01/101", "MP 01/102",
+                              "MP 01/110", "MP 01/111", "MP 01/112", "MP 01/122", "MP 01/138", 
+                              "MP 01/142", "MP 01/144", "MP 01/145", "MP 01/146",  "MP 01/147", 
+                              "MP 01/154", "MP 01/171", "MP 01/182", "MP 01/194", "MP 01/200", 
+                              "MP 02/073", "MP 02/254"),
+                  selected = ""),
       ),
-    
     mainPanel("",
               fluidRow(
-                #splitLayout(style = "border: 1px solid silver:", cellWidths = c(300,300), 
                             plotOutput("ncplot")
-                            
-                            
                 )
-                
-                
               ))
-    
-    
-    
-    
   )
 
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
+
+  
   
   library(ggplot2)
   Sys.setlocale("LC_ALL", "C")
   output$ncplot <- renderPlot({
-    df<- read_excel("dati.xlsx")
-    df <- df %>% 
+    df <- read_excel("dati.xlsx", col_types = c("text", 
+                                                "text", "text", "numeric", "text", "text", 
+                                                "text", "text", "text", "numeric", "text", 
+                                                "text", "text", "text"))
+    df %>% 
+      filter(sede == input$s) %>%
       group_by(anno, mp) %>% 
       summarise(zscore= mean(zscore, na.rm= TRUE)) %>% 
-      filter(mp == input$mp)  
-    ggplot(df, aes(anno,zscore, group=1))+geom_point()+
+      filter(mp == input$mp)  %>% 
+    ggplot(aes(anno,zscore, group=1))+geom_point()+
       scale_y_continuous(breaks = c(-3,-2,-1,0,1,2,3))+
       geom_line(linetype=1,size=0.2)+ggtitle(input$mp)+
       geom_hline(yintercept=c(-3,-2,-1,0,1,2,3), col='red', size=0.2, alpha=0.6)+
